9a0b58e91cbc4f2c77ee812174f89f703dd8d0e6
Pacifique Linjanja <pacilinja1@icloud.com>
2020-05-16 10:15:49 +0200

    Adding validation message logic

--- src/controller/users.controller.js
+++ src/controller/users.controller.js
@@ -14,9 +14,10 @@ exports.signup = (req, res, next) => {
                 mail : req.body.mail,
                 msgCode : Math.floor(Math.random()*999)+1000
             })
-            user.save()
-                .then(user => res.status(200).json({ user }))
-                .catch(error => res.status(400).json({ error }))
+            res.status(200).json({ user })
+            // user.save()
+            //     .then(user => res.status(200).json({ user }))
+            //     .catch(() => res.status(400).json({ errorMessage : 'Adresse mail ou numero de telephone deja existant, avez-vous deja un compte MAgoEat ?' }))
         })
         .catch(error => res.status(500).json({ error }))
 }
@@ -46,6 +47,24 @@ exports.login = (req, res, next) => {
         .catch(error => res.status(500).json({ error }))
 }
 
+exports.consfirmSms = (req, res, next) => {
+    if (!req.body.msgCode) {
+        res.status(400).json({ errorMessage : 'Aucune session est ouverte'})
+    }
+        // then we can save the user ...
+    const user = new User ({
+        username : req.body.username,
+        password : hash,
+        phone : req.body.phone,
+        adress : req.body.adress,
+        mail : req.body.mail,
+        msgCode : Math.floor(Math.random()*999)+1000
+    })
+    user.save()
+        .then(user => res.status(200).json({ user }))
+        .catch(() => res.status(400).json({ errorMessage : 'Adresse mail ou numero de telephone deja existant, avez-vous deja un compte MAgoEat ?' }))
+}
+
 exports.sendMsgConf = (req, res, next) => {
     var phone = req.body.phone;
     var username = "pacyL20";
@@ -53,14 +72,14 @@ exports.sendMsgConf = (req, res, next) => {
     var source = "MAGOEAT APP";
     var msg = req.body.msgDetail +" "+ req.body.msgCode;
         
-    request1('http://localhost:8080/bulksms/bulksms?username='+username+'&password='+password+'&type=0&dlr=1&destination='+phone+'&source='+source+'&message='+msg, function (error1, response1, body1) {
-        console.error('error:', error1); // Print the error if one occurred
-        console.log('statusCode:', response1 && response1.statusCode);
-        console.log('body:', body1); 
-        response.status(response1.statusCode).send({ "message": "Code envoye avec success"});
+    request1('http://api.rmlconnect.net/bulksms/bulksms?username='+username+'&password='+password+'&type=0&dlr=1&destination='+phone+'&source='+source+'&message='+msg, function (error1, response1, body1) {
+        // console.error('error:', error1); // Print the error if one occurred
+        // console.log('statusCode:', response1 && response1.statusCode);
+        // console.log('body:', body1); 
+        res.status(response1.statusCode).send({ "message": "Votre code de confiramtion a ete envoye avec succes, veuillez verifier vos messages entrants"});
     });
 }
 
-exports.comparePassword = (req, res, next) => {
+// exports.comparePassword = (req, res, next) => {
 
-}
+// }
--- src/router/users.route.js
+++ src/router/users.route.js
@@ -5,5 +5,6 @@ const userCtl = require('../controller/users.controller')
 router.post('/signup', userCtl.signup)
 router.post('/login', userCtl.login)
 router.post('/msgConf', userCtl.sendMsgConf)
+router.post('/validateSms', userCtl.consfirmSms)
 
 module.exports = router
