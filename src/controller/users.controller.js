805b28b8fac8aa51818d1c4272b92aa71f920a73
Pacifique Linjanja <pacilinja1@icloud.com>
2020-08-09 14:31:33 +0200

    Commenting unused features

--- src/controller/users.controller.js
+++ src/controller/users.controller.js
@@ -63,7 +63,7 @@ exports.consfirmSms = (req, res, next) => {
         msgCode : req.body.msgCode
     })
     user.save()
-        .then(user => res.status(200).send({ user }))
+        .then((user) => res.status(200).send({ user }))
         .catch((error) => res.status(400).send({ error }))
 }
 
@@ -87,29 +87,29 @@ exports.sendMsgConf = (req, res, next) => {
     });
 }
 
-exports.sendMsgToAdmins = (req, res, next) => {
-    var username = "pacyL20";
-    var password = "zKssVK4u";
-    var source = "MAGOEAT APP ADMIN";
-    var msg = "Bonjour, +"+req.body.msgPhoneClient+ " au pseudo "+req.body.username+" viens de passer une commande de"+req.body.quantity+" plats de "+req.body.repas+" chez "+req.body.restau; 
-    request1('http://api.rmlconnect.net/bulksms/bulksms?username='+username+'&password='+password+'&type=0&dlr=1&destination=243990831772&source='+source+'&message='+msg, function (error1, response1, body1) {
-        res.status(response1.statusCode).json({
-            message : 'Envoie de la commande reussi, le traitement est en cours',
-            date : Date()
-        })
-    });
-}
+// exports.sendMsgToAdmins = (req, res, next) => {
+//     var username = "pacyL20";
+//     var password = "zKssVK4u";
+//     var source = "MAGOEAT APP ADMIN";
+//     var msg = "Bonjour, +"+req.body.msgPhoneClient+ " au pseudo "+req.body.username+" viens de passer une commande de"+req.body.quantity+" plats de "+req.body.repas+" chez "+req.body.restau; 
+//     request1('http://api.rmlconnect.net/bulksms/bulksms?username='+username+'&password='+password+'&type=0&dlr=1&destination=243990831772&source='+source+'&message='+msg, function (error1, response1, body1) {
+//         res.status(response1.statusCode).json({
+//             message : 'Envoie de la commande reussi, le traitement est en cours',
+//             date : Date()
+//         })
+//     });
+// }
 
-exports.getUserByUsername = (req, res, next) => {
-    User.findOne({username : req.params.username})
-        .then(user => {
-            if (user) {
-                res.status(200).json({ user })
-            }
-            res.status(401).json({ message : 'Uilisateur introuvable' })
-        })
-        .catch(() => res.status(400).json({ errorMessage : 'utilisateur non trouvE'}))
-}
+// exports.getUserByUsername = (req, res, next) => {
+//     User.findOne({username : req.params.username})
+//         .then(user => {
+//             if (user) {
+//                 res.status(200).json({ user })
+//             }
+//             res.status(401).json({ message : 'Uilisateur introuvable' })
+//         })
+//         .catch(() => res.status(400).json({ errorMessage : 'utilisateur non trouvE'}))
+// }
 // exports.comparePassword = (req, res, next) => {
 
 // }
--- src/router/users.route.js
+++ src/router/users.route.js
@@ -6,10 +6,10 @@ router.post('/signup', userCtl.signup)
 router.post('/login', userCtl.login)
 router.post('/msgConf', userCtl.sendMsgConf)
 router.post('/validateSms', userCtl.consfirmSms)
-router.post('/sendToAdmins', userCtl.sendMsgToAdmins)
+// router.post('/sendToAdmins', userCtl.sendMsgToAdmins)
 
 // route to get connected user
 
-router.get('/login/:username', userCtl.getUserByUsername)
+// router.get('/login/:username', userCtl.getUserByUsername)
 
 module.exports = router
