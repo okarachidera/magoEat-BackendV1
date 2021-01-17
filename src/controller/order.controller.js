8a5f1a753c869f213135ee913352dc0dc20e8149
Pacifique Linjanja <pacilinja1@icloud.com>
2021-01-17 15:10:00 +0100

    Put the update order

--- src/controller/order.controller.js
+++ src/controller/order.controller.js
@@ -43,42 +43,42 @@ exports.getAllOrders = (req, res) => {
 
 // For post routes
 
-// exports.rateOrder = (req, res, next) => {
-//     /**
-//      * First we check if the order exists.Finding it in the model
-//      */
-//     Order.findOne({ id_ : req.params.id })
-//         .then(order => {
-//             if (!order) {
-//                 res.status(400).json({ errorMessage : 'Erreur, cet article n\'existe pas '})
-//             }
-//             /**
-//              * Then we check if its ratable field is true
-//              */
-//             if (!order.ratable) {
-//                 res.status(400).json({ errorMessage : 'Cette commande ne peut etre cotee maintenant'})
-//             }
-//             /**
-//              * Then we can update its rate and its ratable fields
-//              */
-//             Order.updateOne({ id_ : req.body.id }, { ratable : false , rate : req.body.rate })
-//                 .then(orderUpdated => {
-//                     /**
-//                      * Let's check if the response is not empty
-//                      */
-//                     if (!orderUpdated) {
-//                         res.status(400).json({ errorMessage : 'Erreur de mise a jour de votre cotaton'})
-//                     }
-//                     res.status(200).json({ orderUpdated })
-//                 })
-//                 .catch(error => {
-//                     console.log(error)
-//                 })
-//         })
-//         .catch(error => {
-//             console.log(error)
-//         })
-// }
+exports.rateOrder = (req, res, next) => {
+    /**
+     * First we check if the order exists.Finding it in the model
+     */
+    Order.findOne({ id_ : req.params.id })
+        .then(order => {
+            if (!order) {
+                res.status(400).json({ errorMessage : 'Erreur, cet article n\'existe pas '})
+            }
+            /**
+             * Then we check if its ratable field is true
+             */
+            if (!order.ratable) {
+                res.status(400).json({ errorMessage : 'Cette commande ne peut etre cotee maintenant'})
+            }
+            /**
+             * Then we can update its rate and its ratable fields
+             */
+            Order.updateOne({ id_ : req.body.id }, { ratable : false , rate : req.body.rate })
+                .then(orderUpdated => {
+                    /**
+                     * Let's check if the response is not empty
+                     */
+                    if (!orderUpdated) {
+                        res.status(400).json({ errorMessage : 'Erreur de mise a jour de votre cotaton'})
+                    }
+                    res.status(200).json({ orderUpdated })
+                })
+                .catch(error => {
+                    console.log(error)
+                })
+        })
+        .catch(error => {
+            console.log(error)
+        })
+}
 
 exports.placeOrder = (req, res) => {
     // First, let's validate the request
