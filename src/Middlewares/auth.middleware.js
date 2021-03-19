7f80fb17148a83208938e57cb03a36bccc3f1b32
Pacifique LINJANJA <pacilinja1@icloud.com>
2021-03-19 01:22:19 +0100

    MInor update

--- src/Middlewares/auth.middleware.js
+++ src/Middlewares/auth.middleware.js
@@ -1,1 +1,24 @@
 // To be implemented, protected routes baed on the auth feature
+const jwt = require("jsonwebtoken");
+const config = require("config");
+
+module.exports = (req, res, next) => {
+    const token = req.headers["x-access-token"] || req.headers["authorization"];
+
+    if ( !token ) {
+        return res.status(404).json({
+            success: false,
+            message: "Veuillez vous rassurer que vous etes bien connecte"
+        });
+    }
+
+    try {
+        const decoded = jwt.verify(token, config.get("myprivatekey"));
+        req.user = decoded;
+        next();
+    } catch (ex) {
+        //if invalid token
+        res.status(400).send("Invalid token.");
+    }
+
+};
