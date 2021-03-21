0704fe42ef818b03f38fbf9f7846ace6d6a628bb
Pacifique LINJANJA <pacilinja1@icloud.com>
2021-03-21 12:22:59 +0100

    Add the create category validation

--- src/validators/category.validators.js
+++ src/validators/category.validators.js
@@ -1,7 +1,13 @@
-// const Joi = require("@hapi/joi");
+const Joi = require("@hapi/joi");
 
-// const validLabel = Joi.string()
-//     .min(3)
-//     .max(25);
-// const validUrl = Joi.string()
-//     .max(255)
+const validLabel = Joi.string()
+    .min(3)
+    .max(25);
+const validUrl = Joi.string()
+    .max(255);
+
+exports.createUpdateCategory = Joi.object().keys({
+    label: validLabel.required(),
+    imgCroped: validUrl.required(),
+    imgBrushed: validUrl.required()  
+});
