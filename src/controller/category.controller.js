002aebe3ccc3686fd85465c444f36393db72181d
Pacifique LINJANJA <pacilinja1@icloud.com>
2021-03-21 13:30:30 +0100

    Add the update method on the controller

--- src/controller/category.controller.js
+++ src/controller/category.controller.js
@@ -1,6 +1,23 @@
 const Category = require("../models/category.model");
 const categoryValidator = require("../validators/category.validators");
 
+exports.index = (req, res) => {
+    Category.find({}, (err, categories) => {
+        if (!err) {
+            res.status(200)
+                .json({
+                    success: true,
+                    categories
+                });
+        }
+        res.status(500)
+            .json({
+                success: false,
+                err
+            });
+    });
+};
+
 exports.createCategory = (req, res) => {
     const {error, value} = categoryValidator.createUpdateCategory.validate(req.body);
     if (error) {
@@ -31,4 +48,39 @@ exports.createCategory = (req, res) => {
                     err
                 });
         });
+};
+
+exports.updateCategory = (req, res) => {
+    const {error, value} = categoryValidator.createUpdateCategory.validate(req.body);
+    if (error) {
+        res.status(500)
+            .json({
+                success: false,
+                message: "Mise a jour echouee",
+                error
+            });
+    }
+    Category.findOne({label: value.label})
+        .then(cat => {
+            cat.update({
+                label: value.label ? value.label : cat.label,
+                imgRed: value.imgRed ? value.imgRed : cat.imgRed,
+                imgWhite: value.imgWhite ? value : cat.imgWhite
+            })
+                .then(c => {
+                    res.status(202).json({
+                        success: true,
+                        message: "Mise a jour reussie",
+                        category: c
+                    });
+                })
+                .catch(err => {
+                    res.status(500).json({
+                        success: false,
+                        message: "Mise a jour echouee",
+                        err
+                    });
+                });
+        })
+        .catch();
 };
