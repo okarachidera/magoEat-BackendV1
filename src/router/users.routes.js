cb1c1cdd2db83f7d5e40ef3c15f1268fd1764d35
Pacifique LINJANJA <pacilinja1@icloud.com>
2021-03-29 15:21:14 +0200

    Minor update

--- src/router/users.routes.js
+++ src/router/users.routes.js
@@ -6,14 +6,18 @@ router.post("/signup", userCtl.signup);
 router.post("/login", userCtl.login);
 router.post("/msgConf", userCtl.sendMsgConf);
 router.post("/validateSms", userCtl.consfirmSms);
-// router.post('/sendToAdmins', userCtl.sendMsgToAdmins)
+// router.post('/sendToAdmins', userCtl.sendMsgToAdmins);
 
 // route to get connected user
 
 router.get("/user/:username", userCtl.getUserByUsername);
 // to get all the users 
 router.get("/users", userCtl.getAllUsers);
-// to get all owners 
+
+/**
+ * @route:  
+ * Get complet list of owners of restaurants
+ */
 router.get("/owners", userCtl.getOwners);
 
 module.exports = router;
