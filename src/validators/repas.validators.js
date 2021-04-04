a2112a31a026e7c25a1df97267ebbbfdb7968ffa
Pacifique LINJANJA <pacilinja1@icloud.com>
2021-04-04 20:12:07 +0200

    MInor update

--- src/validators/repas.validators.js
+++ src/validators/repas.validators.js
@@ -14,9 +14,6 @@ const rate = Joi.number()
 const url = Joi.string()
     .alphanum()
     .optional();
-const validCategory = Joi.string()
-    .valid("CONGOLESE", "CHINEESE", "FRENCH", "ITALIAN", "VEGETARIAN", "TAKE_AWAY")
-    .required();
 const charge = Joi.number()
     .max(1)
     .min(0);
@@ -26,8 +23,8 @@ exports.createRepas = Joi.object().keys({
     idRestau: validId.required(),
     description: textField.required(),
     imgUrl: textField.required(),
-    category: validCategory,
-    subCategory: Joi.string().optional(),
+    category: validId.required(),
+    subCategory: validId.required(),
     timeForCook: Joi.number(),
     price: Joi.number().required(),
     charge
